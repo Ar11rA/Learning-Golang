8fce8f28e77f186b1f925c7c0b6f578edaa063c9
Aritra Ghosh <aritra119.26@gmail.com>
2019-04-04 17:05:59 +0530

    added sync and async examples for remote callers

---
+++ General/remote_caller_sync.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"text/template"
+	"time"
+
+	"github.com/gojektech/heimdall/httpclient"
+)
+
+type data struct {
+	Name  string
+	Value int
+}
+
+func getResourceContents(path string) []byte {
+	f, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("os.Open() failed with '%s'\n", err)
+	}
+	defer f.Close()
+
+	d, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Fatalf("ioutil.ReadAll() failed with '%s'\n", err)
+	}
+	return d
+}
+
+func formRequest(contents []byte, d data) *bytes.Buffer {
+	t := template.New("request_template")
+	s := string(contents)
+	t, _ = t.Parse(s)
+	buf := &bytes.Buffer{}
+	err := t.Execute(buf, d)
+	if err != nil {
+		log.Fatalf("templating failed with '%s'\n", err)
+	}
+	log.Println("Final request:", buf.String())
+	return buf
+}
+
+func makePostRequest(remoteURL string, buf *bytes.Buffer) string {
+	timeout := 1000 * time.Millisecond
+	client := httpclient.NewClient(httpclient.WithHTTPTimeout(timeout))
+	buff := []io.Reader{buf}
+	combined := io.MultiReader(buff...)
+	headers := http.Header{}
+	headers.Set("Content-Type", "application/json")
+	response, _ := client.Post(remoteURL, combined, headers)
+	byteResponse, _ := ioutil.ReadAll(response.Body)
+	return string(byteResponse)
+}
+
+func main() {
+	path := "./resources/request_template.txt"
+	data1 := data{
+		Name:  "def",
+		Value: 10,
+	}
+	data2 := data{
+		Name:  "abc",
+		Value: 5,
+	}
+	data3 := data{
+		Name:  "xyz",
+		Value: 15,
+	}
+	fileContents := getResourceContents(path)
+	requestTemplate1 := formRequest(fileContents, data1)
+	requestTemplate2 := formRequest(fileContents, data2)
+	requestTemplate3 := formRequest(fileContents, data3)
+	res1 := makePostRequest("http://localhost:3000/ping", requestTemplate1)
+	res2 := makePostRequest("http://localhost:3000/ping", requestTemplate2)
+	res3 := makePostRequest("http://localhost:3000/ping", requestTemplate3)
+	log.Println(res1)
+	log.Println(res2)
+	log.Println(res3)
+}
